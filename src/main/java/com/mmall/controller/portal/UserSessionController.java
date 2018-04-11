e422a0a90f84ee811f62cbcae06d4779054b35b3
GeforceLee <314004856@qq.com>
2018-04-11 11:36:39 +0800

    fix success return data bug

--- src/main/java/com/mmall/controller/portal/UserSessionController.java
+++ src/main/java/com/mmall/controller/portal/UserSessionController.java
@@ -105,7 +105,7 @@ public class UserSessionController {
         if (user == null){
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEES_LOGIN.getCode(),ResponseCode.NEES_LOGIN.getDesc());
         }
-        return ServerResponse.createByErrorMessage("用户未登录，无法获取用户信息");
+        return ServerResponse.createBySuccess(user);
     }
 
 
