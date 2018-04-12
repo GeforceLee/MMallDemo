c20613c24f708020db5133fa9339f6fa2797211e
GeforceLee <314004856@qq.com>
2018-04-12 17:24:33 +0800

    增加config

--- src/main/java/com/mmall/config/ApplicationConfig.java
+++ src/main/java/com/mmall/config/ApplicationConfig.java
@@ -1,11 +1,13 @@
 package com.mmall.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author geforce
  * @date 2018/4/12
  */
+@Configuration
 @EnableScheduling
 public class ApplicationConfig {
 }
